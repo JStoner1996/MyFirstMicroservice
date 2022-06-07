abe7271a4ae44db77aae12221312ece6c6939935
Jake Stoner <jakestoner123@live.com>
2022-06-07 09:52:40 +0100

    added mockito tests

--- src/main/java/com/tsi/jake/stoner/program/MyFirstMicroserviceApplication.java
+++ src/main/java/com/tsi/jake/stoner/program/MyFirstMicroserviceApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @CrossOrigin (origins = "*") // needed for receiving request via api
 @SpringBootApplication
 @RestController // Handles GET, POST, DELETE, PUT requests
@@ -16,22 +18,41 @@ public class MyFirstMicroserviceApplication {
 	private ActorRepository actorRepository;
 	@Autowired
 	private FilmRepository filmRepository;
+
+
+	private String saved = "Saved";
 	public static void main(String[] args) {
 		SpringApplication.run(MyFirstMicroserviceApplication.class, args);
-
 	}
 
 	public MyFirstMicroserviceApplication(ActorRepository actorRepository, FilmRepository filmRepository){
 		this.actorRepository = actorRepository;
 		this.filmRepository = filmRepository;
-	}
 
+	}
 
 
 
 	@GetMapping("/All_Actors")
 	public @ResponseBody
 	Iterable<Actor>getAllActors(){
-		return actorRepository.findAll();
+	return actorRepository.findAll();
 	}
+
+	@PostMapping("/addActor")
+	public @ResponseBody String addActor (@RequestParam String firstName, String lastName){
+		Actor addActor = new Actor("Leonard", "DeSagi");
+		actorRepository.save(addActor);
+		return saved;
+	}
+
+	@GetMapping("/All_Films")
+	public @ResponseBody
+	Iterable<Film>getAllFilms(){
+		return filmRepository.findAll();
+	}
+
+
+
+
 }
---
+++ src/test/java/com/tsi/jake/stoner/program/MockitoTest.java
@@ -0,0 +1,51 @@
+package com.tsi.jake.stoner.program;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class MockitoTest {
+
+    private MyFirstMicroserviceApplication myFirstMicroserviceApplication;
+    @Mock
+    private ActorRepository actorRepository;
+    @Mock
+    private FilmRepository filmRepository;
+
+
+
+    // Set's up temp db
+    @BeforeEach
+    void setUp(){
+        myFirstMicroserviceApplication = new MyFirstMicroserviceApplication(actorRepository, filmRepository);
+    }
+
+    @Test
+    public void getAllActors(){
+        myFirstMicroserviceApplication.getAllActors();
+        verify(actorRepository).findAll();
+    }
+
+    @Test
+    public void getAllFilms(){
+        myFirstMicroserviceApplication.getAllFilms();
+        verify(filmRepository).findAll();
+    }
+
+    @Test
+    public void testAddActor(){
+        Actor newActor = new Actor("Tom", "Shanks");
+        String expected = "Saved";
+        String Actual = myFirstMicroserviceApplication.addActor(newActor.getFirstName(), newActor.getLastName());
+        ArgumentCaptor<Actor> actorArgumentCaptor = ArgumentCaptor.forClass(Actor.class);
+        verify(actorRepository).save(actorArgumentCaptor.capture());
+        actorArgumentCaptor.getValue();
+        Assertions.assertEquals(expected, Actual, "Actor not saved in Database.");
+    }
+}
