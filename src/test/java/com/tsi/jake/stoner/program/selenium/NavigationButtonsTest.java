007e53284a90250a169a63732888aca1bd8a2dd3
Jake Stoner <jakestoner123@live.com>
2022-06-21 14:10:29 +0100

    changes

--- src/test/java/com/tsi/jake/stoner/program/selenium/NavigationButtonsTest.java
+++ src/test/java/com/tsi/jake/stoner/program/selenium/NavigationButtonsTest.java
@@ -3,10 +3,7 @@ package com.tsi.jake.stoner.program.selenium;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.openqa.selenium.By;
@@ -19,7 +16,7 @@ public class NavigationButtonsTest {
     String Actual;
 
     private final String HOME_URL= "http://localhost:3000/";
-    // Creates new window for selenium to use before each test is ran
+    // Creates new window for selenium to use before each test is run
     @BeforeEach
     public void setUp() {
         WebDriverManager.chromedriver().setup();
@@ -33,73 +30,64 @@ public class NavigationButtonsTest {
     }
 
     // Uses parameterized tests instead of multiple tests, 1st Value is ID, 2nd is word used for URL Actual
-    @ParameterizedTest
+//    @ParameterizedTest
+//    @CsvSource({
+//            "goToActorButton, Actor",
+//            "goToCategoryButton, Category",
+//            "goToKeywordButton, Keyword",
+//            "goToLanguageButton, Language",
+//            "goToLengthButton, Length"
+//    })
+//    void testNavButtons(String id, String word){
+//        driver.get("http://localhost:3000/");
+//
+//        driver.findElement(By.id(id)).click();
+//        String URL = driver.getCurrentUrl();
+//        Assertions.assertEquals(URL, HOME_URL + word, "Incorrect Page");
+//        driver.quit();
+//    }
 
-    @CsvSource({
-            "goToActorButton, Actor",
-            "goToCategoryButton, Category",
-            "goToKeywordButton, Keyword",
-            "goToLanguageButton, Language",
-            "goToLengthButton, Length"
-    })
-    void testNavButtons(String id, String word){
+
+    @Test
+    public void testActorNav() {
         driver.get("http://localhost:3000/");
-
-        driver.findElement(By.id(id)).click();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        driver.findElement(By.id("goToActorButton")).click();
         String URL = driver.getCurrentUrl();
-        Assertions.assertEquals(URL, HOME_URL + word, "Incorrect Page");
-        driver.quit();
-
+        Assertions.assertEquals(URL, HOME_URL + "Actor", "Incorrect Page");
+    }
+    @Test
+    public void testCategoryNav() {
+        driver.get("http://localhost:3000/");
+        driver.findElement(By.id("goToCategoryButton")).click();
+        String URL = driver.getCurrentUrl();
+        Assertions.assertEquals(URL, HOME_URL + "Category", "Incorrect Page");
     }
 
 
+    @Test
+    public void testKeywordNav() {
+        driver.get("http://localhost:3000/");
+        driver.findElement(By.id("goToKeywordButton")).click();
+        String URL = driver.getCurrentUrl();
+        Assertions.assertEquals(URL, HOME_URL + "Keyword", "Incorrect Page");
+    }
 
-//
-//    @Test
-//    public void testActorNav() {
-//        driver.get("http://localhost:3000/");
-//        driver.findElement(By.id("goToActorButton")).click();
-//        String URL = driver.getCurrentUrl();
-//        Assertions.assertEquals(URL, HOME_URL + "Actor", "Incorrect Page");
-//    }
-//    @Test
-//    public void testCategoryNav() {
-//        driver.get("http://localhost:3000/");
-//        driver.findElement(By.id("goToCategoryButton")).click();
-//        String URL = driver.getCurrentUrl();
-//        Assertions.assertEquals(URL, HOME_URL + "Category", "Incorrect Page");
-//    }
-//
-//
-//    @Test
-//    public void testKeywordNav() {
-//        driver.get("http://localhost:3000/");
-//        driver.findElement(By.id("goToKeywordButton")).click();
-//        String URL = driver.getCurrentUrl();
-//        Assertions.assertEquals(URL, HOME_URL + "Keyword", "Incorrect Page");
-//    }
-//
-//    @Test
-//    public void testLanguageNav() {
-//        driver.get("http://localhost:3000/");
-//        driver.findElement(By.id("goToLanguageButton")).click();
-//        String URL = driver.getCurrentUrl();
-//        Assertions.assertEquals(URL, HOME_URL + "Language", "Incorrect Page");
-//    }
-//
-//    @Test
-//    public void testLengthNav() {
-//        driver.get("http://localhost:3000/");
-//        driver.findElement(By.id("goToLengthButton")).click();
-//        String URL = driver.getCurrentUrl();
-//        System.out.println("Hello");
-//        Assertions.assertEquals(URL, HOME_URL + "Length", "Incorrect Page");
-//    }
+    @Test
+    public void testLanguageNav() {
+        driver.get("http://localhost:3000/");
+        driver.findElement(By.id("goToLanguageButton")).click();
+        String URL = driver.getCurrentUrl();
+        Assertions.assertEquals(URL, HOME_URL + "Language", "Incorrect Page");
+    }
+
+    @Test
+    public void testLengthNav() {
+        driver.get("http://localhost:3000/");
+        driver.findElement(By.id("goToLengthButton")).click();
+        String URL = driver.getCurrentUrl();
+        System.out.println("Hello");
+        Assertions.assertEquals(URL, HOME_URL + "Length", "Incorrect Page");
+    }
 
 
 // // Used to slow down executions
